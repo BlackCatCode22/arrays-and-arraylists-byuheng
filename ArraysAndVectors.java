5534bcb9042b028094d0a3fd45a266da78c91f98
byuheng <141770040+byuheng@users.noreply.github.com>
2023-08-24 14:18:28 -0700

    ArraysAndVectors.java

--- ArraysAndVectors.java
+++ ArraysAndVectors.java
@@ -1,15 +1,13 @@
-    // ArraysAndVectors.java
-    // Heng Ky
-    // 24/08/23
-
-
+// ArraysAndVectors.java
+// Heng Ky
+// 24/08/23
 
 public class ArraysAndVectors {
     public static void main(String[] args) {
 
-        System.out.println("\n Welcome to the Array Program! \n");
+        System.out.println("\n Welcome to the Array Program!\n");
 
-        // Create an in array = sign mean "assignment operator"
+        // Create an int array
         int[] myNumbers = new int[8];
 
         // Fill the array with ints
@@ -22,52 +20,53 @@ public class ArraysAndVectors {
         myNumbers[6] = 13;
         myNumbers[7] = 15;
 
-        // Output the array
+        // output the array
         System.out.println("myNumbers[3] = " + myNumbers[3]);
 
+        System.out.println("\n\n");
 
-        for (int i = 0; i < 8; i++) {
-            // Print the elements of the array
+        for (int i = 0; i < 8; i++ ) {
+            // print the elements of the array
             System.out.println("myNumbers[" + i + "] is: " + myNumbers[i]);
         }
 
-        // Fill the array with 5, 10, 15, etc.
+        // fill the array with 5, 10 , 15, etc.
         int myFiveContainer = 0;
         for (int i = 0; i < 8; i++) {
-            // Fill the elements of the array
+            // fill the elements of the array
             myNumbers[i] = myFiveContainer += 5;
         }
-        System.out.println("\n\n This array should be the fives: ");
-        for (int i = 0; i < 8; i++) {
-            // Print the elements of the array
+
+        System.out.println("\n\n this array should be the fives: ");
+        for (int i = 0; i < 8; i++ ) {
+            // print the elements of the array
             System.out.println("myNumbers[" + i + "] is: " + myNumbers[i]);
         }
-        for (int i = 0; i < 3; i++) {
-            for (int j = 0; j < 4; j++) {
-                System.out.println("i= " + i + ", j= " + j);
-            }
-            // Create a 2D array
-            int[][] myNumberGrid = new int[4][3] {
 
-                // Fill the array with a for() loop!
-                myFiveContainer = 0;
-                for (int i = 0; i < 4; i++) {
-                    for (int j = 0; j > 3; j++) {
-                        myNumberGrid[i][j] = myFiveContainer += 5;
-                    }
-                }
+        // Create a 3D array
+        int[][][] myNumberGrid = new int[100][4][3];
 
-                // Output the array with a for() loop!
-                for (int i = 0; i < 4; i++) {
-                    for (int j = 0; j > 3; j++) {
-
-                        System.out.print(myNumberGrid[i][j] + " ");
-                    }
-                    System.out.println("");
-
+        // fill the array with a for() loop!
+        myFiveContainer = 0;
+        for (int h = 0; h < myNumberGrid.length; h++){
+            for (int i = 0; i < myNumberGrid[h].length; i++) {
+                for (int j = 0; j < myNumberGrid[h][i].length; j++) {
+                    myNumberGrid[h][i][j] = myFiveContainer += 5;
                 }
             }
-
         }
+
+        // output the array with a for() loop!
+        System.out.println("\n\n");
+        for (int h=0; h<100; h++) {
+            for (int i = 0; i < 4; i++) {
+                for (int j = 0; j < 3; j++) {
+                    System.out.print(myNumberGrid[h][i][j] + " ");
+                }
+                System.out.println("");
+            }
+            System.out.println("");
+        }
+
     }
 }
